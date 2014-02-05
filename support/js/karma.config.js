2204ef02ee40fc1512db34b9b0837bdc56f3aef5
Chris Dawson <xrdawson@gmail.com>
2014-02-04 17:29:51 -0800

    Added support files

---
+++ support/js/karma.config.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+        'angular.js',
+        'fixtures-*.js',
+        'angular-mocks.js',
+        'github.js',
+        '*.js'
+    ],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    captureTimeout: 60000,
+    singleRun: false
+  });
+};
