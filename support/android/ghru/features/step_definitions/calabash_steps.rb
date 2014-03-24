b8e957f537a249355deff7abd69c372cdb11197b
Chris Dawson <xrdawson@gmail.com>
2014-03-24 12:19:19 -0700

    Moving to ghru

---
+++ support/android/ghru/features/step_definitions/calabash_steps.rb
@@ -0,0 +1,15 @@
+require 'calabash-android/calabash_steps'
+
+def check_and_set( id, text )
+  check_element_exists "edittext id:'#{id}'"
+  query "edittext id:'#{id}'", :setText => text
+end
+
+When(/^I enter the username$/) do
+  check_and_set( "username", ENV['GH_USERID'] )
+end
+
+When(/^I enter the password$/) do
+  check_and_set( "password", ENV['GH_PASSWORD'] )
+end
+
