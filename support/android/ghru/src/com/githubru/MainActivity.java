b83a9a7147c9b0a9d8bc9b726b24448a0ca36f84
Chris Dawson <xrdawson@gmail.com>
2014-03-27 15:48:34 -0700

    Add callouts to new Java code

--- support/android/ghru/src/com/githubru/MainActivity.java
+++ support/android/ghru/src/com/githubru/MainActivity.java
@@ -10,36 +10,35 @@ import android.view.View;
 
 public class MainActivity extends Activity
 {
-    private LinearLayout ll = null;
-
-    private void login() {
-        setContentView(R.layout.logged_in);
-
-        Button submit = (Button)findViewById( R.id.submit );
-        submit.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    doPost();
-                }
-            });
-    }
-
-    private void doPost() {
-        TextView tv = (TextView)findViewById( R.id.status );
-        tv.setText( "Successful jekyll post" );
-    }
-
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main); 
-        Button login = (Button)findViewById( R.id.login );
+
+        Button login = (Button)findViewById( R.id.login ); // <1>
         login.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    login();
+                    login(); // <2>
                 }
             });
     }
+
+    private void login() {
+        setContentView(R.layout.logged_in); // <3>
+
+        Button submit = (Button)findViewById( R.id.submit );
+        submit.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    doPost(); // <4>
+                }
+            });
+    }
+
+    private void doPost() {
+        TextView tv = (TextView)findViewById( R.id.status ); // <5>
+        tv.setText( "Successful jekyll post" );
+    }
+
 }
