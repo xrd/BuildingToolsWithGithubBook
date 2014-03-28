6a0cb94a798e002376cf759b5bfd0f4930674e25
Chris Dawson <xrdawson@gmail.com>
2014-03-28 09:43:07 -0700

    Update feature and steps to involve hitting github api to verify files

--- support/android/ghru/features/step_definitions/calabash_steps.rb
+++ support/android/ghru/features/step_definitions/calabash_steps.rb
@@ -1,5 +1,8 @@
 require 'calabash-android/calabash_steps'
 
+@status = nil
+moods = %w{ happy sad angry blue energized }
+
 def check_and_set( id, text )
   check_element_exists "edittext id:'#{id}'"
   query "edittext id:'#{id}'", :setText => text
@@ -11,5 +14,14 @@ end
 
 When(/^I enter the password$/) do
   check_and_set( "password", ENV['GH_PASSWORD'] )
+end
+
+Then(/^I enter my current mood status$/) do
+  @mood = "Feeling #{moods[(rand()*moods.length).to_i]} today, at #{DateTime.new()}"
+  puts @mood
+end
+
+And(/^I have a new jekyll post/) do
+  
 end
 
