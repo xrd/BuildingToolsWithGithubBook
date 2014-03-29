1b1c6988566ea579a690be0b5acba8a0ee500f2f
Chris Dawson <xrdawson@gmail.com>
2014-03-28 21:24:07 -0700

    Working version of github post

--- support/android/ghru/src/com/githubru/GitHubHelpers.java
+++ support/android/ghru/src/com/githubru/GitHubHelpers.java
@@ -11,17 +11,17 @@ import java.util.*;
 
 class GitHubHelpers {
 
-    public static void SaveFile( String login, String password, String repoName,
-                                   String contentsBase64, String filename, String commitMessage ) {
+    public static boolean SaveFile( String login, String password, String repoName,
+                                   String contentsBase64, String filename ) {
         
         boolean rv = true;
+        String commitMessage = "GitHubRu Update";
 
         try {
             // Thank you: https://gist.github.com/Detelca/2337731
 
             // create needed services
-            RepositoryService repositoryService = new
-            RepositoryService();
+            RepositoryService repositoryService = new RepositoryService();
             repositoryService.getClient().setCredentials( login, password );
             CommitService commitService = new CommitService();
             commitService.getClient().setCredentials( login, password );
@@ -54,10 +54,8 @@ class GitHubHelpers {
             Blob blob = new Blob();
             blob.setContent(contentsBase64);
             blob.setEncoding(Blob.ENCODING_BASE64);
-            String blob_sha = dataService.createBlob(repository,
-            blob);
-            Tree baseTree = dataService.getTree(repository,
-            baseCommitSha);
+            String blob_sha = dataService.createBlob(repository, blob);
+            Tree baseTree = dataService.getTree(repository, baseCommitSha);
 
             // create new tree entry
             TreeEntry treeEntry = new TreeEntry();
@@ -67,11 +65,9 @@ class GitHubHelpers {
 
             treeEntry.setSha(blob_sha);
             treeEntry.setSize(blob.getContent().length());
-            Collection<TreeEntry> entries = new
-            ArrayList<TreeEntry>();
+            Collection<TreeEntry> entries = new ArrayList<TreeEntry>();
             entries.add(treeEntry);
-            Tree newTree = dataService.createTree(repository, entries,
-            baseTree.getSha());
+            Tree newTree = dataService.createTree(repository, entries, baseTree.getSha());
 
             // create commit
             Commit commit = new Commit();
@@ -82,8 +78,7 @@ class GitHubHelpers {
             // listOfCommits.containsAll(base_commit.getParents());
             commit.setParents(listOfCommits);
             // commit.setSha(base_commit.getSha());
-            Commit newCommit = dataService.createCommit(repository,
-            commit);
+            Commit newCommit = dataService.createCommit(repository, commit);
 
             // create resource
             TypedResource commitResource = new TypedResource();
@@ -100,5 +95,6 @@ class GitHubHelpers {
             ieo.printStackTrace();
         }
 
+        return rv;
     }
 }
--- support/android/ghru/src/com/githubru/MainActivity.java
+++ support/android/ghru/src/com/githubru/MainActivity.java
@@ -11,9 +11,14 @@ import android.os.AsyncTask;
 import org.eclipse.egit.github.core.service.UserService;
 import org.eclipse.egit.github.core.User;
 import java.io.IOException;
+import org.apache.commons.codec.binary.Base64;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity
 {
+    String username, password;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -26,8 +31,8 @@ public class MainActivity extends Activity
                 public void onClick(View v) {
                     EditText utv = (EditText)findViewById( R.id.username ); // <1>
                     EditText ptv = (EditText)findViewById( R.id.password );
-                    String username = (String)utv.getText().toString();
-                    String password = (String)ptv.getText().toString();
+                    username = (String)utv.getText().toString();
+                    password = (String)ptv.getText().toString();
                     TextView status = (TextView)findViewById( R.id.login_status ); // <2>
                     status.setText( "Logging in, please wait..." );
                     new LoginTask().execute( username, password ); // <3>
@@ -74,9 +79,50 @@ public class MainActivity extends Activity
     }
 
     private void doPost() {
-        TextView tv = (TextView)findViewById( R.id.post_status ); 
-        tv.setText( "Successful jekyll post" );
-        GitHubHelpers.SaveFile( "", "", "", "", "", "" );
+        new PostTask().execute( username, password );
     }
+
+    class PostTask extends AsyncTask<String, Void, Boolean> {  // <4>
+
+        private String getFilename( String post ) {
+            String title = post.substring( 0, post.length() > 30 ? 30 : post.length() );
+            String jekyllfied = title.toLowerCase().replaceAll( "\\W+", "-").replaceAll( "\\W+$", "" );
+            SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd-" );
+            String prefix = sdf.format( new Date() );
+            String filename = "_posts/" + prefix + jekyllfied + ".md";
+            return filename;
+        }
+
+        @Override 
+            protected Boolean doInBackground(String... credentials) {
+            String username = credentials[0];
+            String password = credentials[1];
+
+            EditText post = (EditText)findViewById( R.id.post );
+            String postContents = post.getText().toString();
+            String base64ed = new String( Base64.encodeBase64( postContents.getBytes() ) );
+
+            EditText repo = (EditText)findViewById( R.id.repository );
+            String repoName = repo.getText().toString();
+
+            String filename = getFilename( postContents );
+
+            return GitHubHelpers.SaveFile( username, password, 
+                                           repoName, base64ed, filename );
+        }
+        
+        @Override
+            protected void onPostExecute(Boolean result) {
+            TextView status = (TextView)findViewById( R.id.post_status );
+            if( result ) {
+                status.setText( "Successful jekyll post" );
+            }
+            else {
+                status.setText( "Post failed." ); // <8>
+            }
+        }
+    }
+
+
     
 }
