6524e4654763b5d6a10d6f9fa6976d813f785c86
Chris Dawson <xrdawson@gmail.com>
2014-04-30 15:55:51 -0700

    Fix script

--- support/gollum/image.rb
+++ support/gollum/image.rb
@@ -2,19 +2,20 @@ require 'sinatra'
 require 'gollum-lib'
 require 'tempfile'
 require 'zip/zip'
+
 wiki = Gollum::Wiki.new(".")
 get '/' do
   render File.open( "index.html" )
 end
+
 post '/unpack' do
-    zip = params[:zip][:tempfile]
-    Zip::ZipFile.open( zip ) { |zipfile|
-      zipfile.each do |f|
-        puts "F: #{f.name}"
-        # Extract files into our images directory                                                                                              filename = "images/" + ( f.name.gsub( /\s+/, '_' ).gsub( /^.*\/([^\/]*)$/, $1 ) )
-        puts "Filename: #{filename}"
-      end
-    }
+  zip = params[:zip][:tempfile]
+  Zip::ZipFile.open( zip ) { |zipfile|
+    zipfile.each do |f|
+      puts "F: #{f.name}"
+      # Extract files into our images directory                                                                                              filename = "images/" + ( f.name.gsub( /\s+/, '_' ).gsub( /^.*\/([^\/]*)$/, $1 ) )
+      puts "Filename: #{filename}"
+    end
   }
   render json: { success: 'ok' }
 end  
