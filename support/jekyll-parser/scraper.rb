2245215d93baafd42d75c43acbdf244ed327556f
Chris Dawson <xrdawson@gmail.com>
2014-06-16 15:03:58 -0700

    Add callouts

--- support/jekyll-parser/scraper.rb
+++ support/jekyll-parser/scraper.rb
@@ -2,23 +2,23 @@ require 'rubygems'
 require 'mechanize'
 require 'vcr'
 
-VCR.configure do |c|
+VCR.configure do |c|  # <1> 
   c.cassette_library_dir = 'cached'
   c.hook_into :webmock
 end
 
 class ByTravelersProcessor
-  attr_accessor :mechanize
+  attr_accessor :mechanize  # <2>
 
   def initialize
-    @mechanize = Mechanize.new { |agent|
+    @mechanize = Mechanize.new { |agent| # <3>
       agent.user_agent_alias = 'Mac Safari'
     }
   end
 
   def run
-    100.times do |i|
-      get_ith_page( i )
+    100.times do |i| 
+      get_ith_page( i ) # <4>
     end
   end
   
