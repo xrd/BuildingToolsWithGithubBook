76574da96e5a3624c7755f09c04593c30356d52c
Chris Dawson <xrdawson@gmail.com>
2015-01-18 15:15:34 -0500

    Updates for broken show sha hashes, ug

--- generate-outline.sh
+++ generate-outline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm full-outline.asciidoc
+rm  -f full-outline.asciidoc
 for x in `ls chapter*.asciidoc`; do 
     echo "== $x" >> full-outline.asciidoc
     grep -h '===\s\S' $x >> full-outline.asciidoc
