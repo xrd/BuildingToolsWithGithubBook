37069390071ad660a4ff93e0fea68648663ea56e
Ben Straub <ben@straub.cc>
2015-10-09 21:38:38 -0700

    A start on .NET testing code

---
+++ support/commit-status/NancyApplication1.Tests/Test.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using Nancy;
+using Nancy.Testing;
+using Nancy.Bootstrapper;
+using System.Collections.Generic;
+using Nancy.Session;
+
+namespace NancyApplication1.Tests
+{
+
+	public static class BootstrapperExtensions
+	{
+		public static void WithSession(this IPipelines pipeline, IDictionary<string, object> session)
+		{
+			pipeline.BeforeRequest.AddItemToEndOfPipeline(ctx =>
+				{
+					ctx.Request.Session = new Session(session);
+					return null;
+				});
+		}
+	}
+
+	[TestFixture ()]
+	public class Test
+	{
+		private ConfigurableBootstrapper bootstrapper;
+		private Browser browser;
+
+		[SetUp]
+		public void Setup(){
+			this.bootstrapper = new ConfigurableBootstrapper();
+			this.browser = new Browser (bootstrapper);
+		}
+
+		[Test ()]
+		public void FetchesUserDetails ()
+		{
+			var result = this.browser.Get ("/mojombo", 
+				with => with.HttpRequest ());
+			Assert.AreEqual (HttpStatusCode.OK, result.StatusCode);
+			Assert.IsTrue (result.Body.AsString()
+				.Contains("Tom Preston-Werner"));
+		}
+
+		[Test]
+		public void HandlesAuthorization()
+		{
+			// Poorly-formatted query
+			bootstrapper.WithSession(new Dictionary<string, object>() { 
+				{ "key", "value" }, 
+				{ "number", 2 } });
+			var result = this.browser.Get ("/authorize", (with) => {
+				with.HttpRequest();
+			});
+			Assert.AreEqual (HttpStatusCode.BadRequest, result.StatusCode);
+		}
+	}
+}
+
