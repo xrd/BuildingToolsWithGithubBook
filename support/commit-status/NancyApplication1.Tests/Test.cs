cb2831e8d0b4b38290c7ee3dad59d1ccb9f74033
Ben Straub <ben@straub.cc>
2015-10-10 10:12:09 -0700

    More .NET tests

--- support/commit-status/NancyApplication1.Tests/Test.cs
+++ support/commit-status/NancyApplication1.Tests/Test.cs
@@ -5,12 +5,15 @@ using Nancy.Bootstrapper;
 using System.Collections.Generic;
 using Nancy.Session;
 
+using NancyApplication1;
+
 namespace NancyApplication1.Tests
 {
 
 	public static class BootstrapperExtensions
 	{
-		public static void WithSession(this IPipelines pipeline, IDictionary<string, object> session)
+		public static void WithSession(this IPipelines pipeline,
+			                           IDictionary<string, object> session)
 		{
 			pipeline.BeforeRequest.AddItemToEndOfPipeline(ctx =>
 				{
@@ -28,7 +31,9 @@ namespace NancyApplication1.Tests
 
 		[SetUp]
 		public void Setup(){
-			this.bootstrapper = new ConfigurableBootstrapper();
+			this.bootstrapper = new ConfigurableBootstrapper(with => {
+				with.Module<Handler>(); 
+			});
 			this.browser = new Browser (bootstrapper);
 		}
 
@@ -45,14 +50,26 @@ namespace NancyApplication1.Tests
 		[Test]
 		public void HandlesAuthorization()
 		{
-			// Poorly-formatted query
-			bootstrapper.WithSession(new Dictionary<string, object>() { 
-				{ "key", "value" }, 
-				{ "number", 2 } });
+			// Mismatched CSRF token
+			bootstrapper.WithSession(new Dictionary<string, object> { 
+				{ "CSRF:State", "sometoken" }, 
+			});
 			var result = this.browser.Get ("/authorize", (with) => {
 				with.HttpRequest();
+				with.Query("state", "someothertoken");
 			});
-			Assert.AreEqual (HttpStatusCode.BadRequest, result.StatusCode);
+			Assert.AreEqual (HttpStatusCode.Unauthorized, result.StatusCode);
+
+			// Matching CSRF token
+			bootstrapper.WithSession(new Dictionary<string, object> { 
+				{ "CSRF:State", "sometoken" },
+				{ "OrigUrl", "http://success" },
+			});
+			result = this.browser.Get ("/authorize", (with) => {
+				with.HttpRequest();
+				with.Query("state", "sometoken");
+			});
+			result.ShouldHaveRedirectedTo ("/authorize/success");
 		}
 	}
 }
